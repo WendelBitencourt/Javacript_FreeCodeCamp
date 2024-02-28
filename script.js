cb6c81442278d2086a6e6729180727b91d4b9adc
Wendel Adriano Bitencourt <wendeladriano@protonmail.com>
2024-02-28 19:32:20 -0300

    Adicionado informações na variavel locations

--- script.js
+++ script.js
@@ -16,7 +16,11 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
-const locations = [];
+const locations = [
+  {
+    name: "town square",
+  },
+];
 
 button1.onclick = goStore;
 button2.onclick = goCave;
