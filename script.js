93d01498b90284de18487662f326cb993e2278f2
Wendel Adriano Bitencourt <wendeladriano@protonmail.com>
2024-03-01 01:08:03 -0300

    Adicionado um novo objeto

--- script.js
+++ script.js
@@ -20,6 +20,19 @@ const locations = [
   {
     name: "town square",
     "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button function": [goStore, goCave, fightDragon],
+    text: 'You are in the town square. You see a sign that says "Store".',
+  },
+
+  {
+    name: "store",
+    "button text": [
+      "Buy 10 health (10 gold)",
+      "Buy weapon (30 gold)",
+      "Go to town square",
+    ],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store.",
   },
 ];
 
