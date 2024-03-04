4b3edc2503c0d4dbabda4ac0b04d6c7f74fb5f56
Wendel Adriano Bitencourt <wendeladriano@protonmail.com>
2024-03-03 22:24:32 -0300

    Modificações

--- script.js
+++ script.js
@@ -54,14 +54,13 @@ Por isso foi criado uma nova função(function update(location)), para encapsula
 
 //Função para encapsular a logica das funções goTown e goStore
 function update(location) {
-  button1.innerText = "Go to store";
-  button2.innerText = "Go to cave";
-  button3.innerText = "Fight dragon";
-  button1.onclick = goStore;
-  button2.onclick = goCave;
-  button3.onclick = fightDragon;
-  text.innerText =
-    'You are in the town square. You see a sign that says "Store".';
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button function"][0];
+  button2.onclick = location["button function"][1];
+  button3.onclick = location["button function"][2];
+  text.innerText = location.text;
 }
 
 function goTown() {
